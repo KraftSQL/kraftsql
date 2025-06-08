00221de584041618f592be86ae5aa2582b8b9d22
Frieler, Christopher <christopher@frieler.rocks>
2025-06-08 22:29:51 +0200

    Document flaw in ColumnExpression

--- kraftsql/src/main/kotlin/rocks/frieler/kraftsql/expressions/ColumnExpression.kt
+++ kraftsql/src/main/kotlin/rocks/frieler/kraftsql/expressions/ColumnExpression.kt
@@ -5,7 +5,7 @@ import rocks.frieler.kraftsql.engine.Engine
 class ColumnExpression<E : Engine<E>, T>(
     val value: Expression<E, T>,
     val alias: String? = null,
-) : Expression<E, T> {
+) : Expression<E, T> { // FIXME: Cannot serve as an Expression in all places, just intended for SELECTs
 
     override fun sql() = value.sql() + if (alias != null) " AS \"$alias\"" else ""
 }
