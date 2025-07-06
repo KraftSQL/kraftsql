199def64d14ddfd261f84cb9a0449c30e11ff9dc
Frieler, Christopher <christopher@frieler.rocks>
2025-07-06 17:09:34 +0200

    Implement an example test for KraftSQL logic

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -10,6 +10,10 @@ dependencies {
 
     testImplementation(libs.junit5.api)
     testImplementation(project(":kraftsql-testing"))
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.kotest.assertions.core)
+    testRuntimeOnly(libs.junit5.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 java {
