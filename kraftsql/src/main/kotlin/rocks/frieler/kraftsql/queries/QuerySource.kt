33bdc9ca8da7bc2d03d6067f5179624d95b08fbf
Frieler, Christopher <christopher@frieler.rocks>
2025-07-13 21:23:35 +0200

    Fix QuerySource without alias

--- kraftsql/src/main/kotlin/rocks/frieler/kraftsql/queries/QuerySource.kt
+++ kraftsql/src/main/kotlin/rocks/frieler/kraftsql/queries/QuerySource.kt
@@ -6,9 +6,9 @@ import rocks.frieler.kraftsql.objects.HasColumns
 
 class QuerySource<E: Engine<E>, T : Any>(
     val data: Data<E, T>,
-    val alias: String?,
+    val alias: String? = null,
 ) : HasColumns<E, T> {
-    fun sql() = "(${data.sql()}) AS \"${alias}\""
+    fun sql() = if (alias != null) "(${data.sql()}) AS \"${alias}\"" else data.sql()
 
     override operator fun <V> get(field: String) = super.get<V>(field)
         .let { if (alias != null) it.withQualifier(alias) else it }
