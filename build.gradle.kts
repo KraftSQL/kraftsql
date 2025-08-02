c250f57fc22306d99a2335f5562b862d21b15d74
Frieler, Christopher <christopher@frieler.rocks>
2025-08-02 20:31:44 +0200

    Provide the version in GitHub Actions workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,17 @@ allprojects {
         mavenCentral()
     }
 }
+
+tasks.register("versionFile") {
+    group = "build"
+    description = "Writes the current package version to 'build/version.txt'."
+
+    val versionTxt = layout.buildDirectory.get().file("version.txt").asFile
+    val version = project.version.toString()
+    doLast {
+        versionTxt.apply {
+            parentFile.mkdirs()
+            writeText(version)
+        }
+    }
+}
