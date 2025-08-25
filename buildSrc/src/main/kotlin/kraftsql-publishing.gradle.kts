ff95b4e78650c1c87df3af5136ef228a586a8b1b
Frieler, Christopher <christopher@frieler.rocks>
2025-08-25 21:31:35 +0200

    Upload release artifacts to maven central

--- buildSrc/src/main/kotlin/kraftsql-publishing.gradle.kts
+++ buildSrc/src/main/kotlin/kraftsql-publishing.gradle.kts
@@ -1,3 +1,7 @@
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
 plugins {
     `maven-publish`
     signing
@@ -33,4 +37,36 @@ signing {
     useInMemoryPgpKeys(
         project.properties["signingKey"]?.toString(),
         project.properties["signingPassword"]?.toString())
+}
+
+tasks.register<Zip>("zipStagedMavenPublication") {
+    description = "Zips the staged Maven publication"
+    dependsOn(tasks["publishMavenPublicationToStagingRepository"])
+
+    from(layout.buildDirectory.dir("staging-deployment"))
+    destinationDirectory = layout.buildDirectory.dir("dist")
+    archiveFileName.set("staged-maven-publication-${project.version}.zip")
+}
+
+tasks.register("uploadToMavenCentral") {
+    group = "publishing"
+    description = "Uploads a release to Maven Central"
+    dependsOn(tasks["zipStagedMavenPublication"])
+
+    val deployment = "${project.group}:${project.name}:${project.version}"
+    val uploadBundle = tasks["zipStagedMavenPublication"].outputs.files.singleFile
+    doLast {
+        val uploadRequest = HttpRequest.newBuilder()
+            .uri(uri("https://central.sonatype.com/api/v1/publisher/upload?name=$deployment&publishingType=AUTOMATIC"))
+            .header("Authorization", "BEARER ${System.getenv("MAVEN_CENTRAL_PUBLISHER_TOKEN")}")
+            .POST(HttpRequest.BodyPublishers.ofFile(uploadBundle.toPath()))
+            .build()
+        val response = HttpClient.newHttpClient().send(uploadRequest, HttpResponse.BodyHandlers.ofString())
+        if (response.statusCode() == 201) {
+            logger.info("Upload to Maven Central succeeded, deployment id is '${response.body()}'")
+        } else {
+            logger.error("Upload to Maven Central failed: ${response.statusCode()}\n${response.body()}")
+            throw IllegalStateException("Upload to Maven Central failed: ${response.statusCode()}")
+        }
+    }
 }
