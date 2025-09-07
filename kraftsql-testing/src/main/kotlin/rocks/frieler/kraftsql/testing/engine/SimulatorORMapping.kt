7c77d776c6ae6b8c226e00003327592aaccfbd6b
Frieler, Christopher <christopher@frieler.rocks>
2025-09-07 17:57:35 +0200

    Fix deserialization of nested structures in SimulatorORMapping

--- kraftsql-testing/src/main/kotlin/rocks/frieler/kraftsql/testing/engine/SimulatorORMapping.kt
+++ kraftsql-testing/src/main/kotlin/rocks/frieler/kraftsql/testing/engine/SimulatorORMapping.kt
@@ -7,6 +7,9 @@ import rocks.frieler.kraftsql.objects.DataRow
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.full.starProjectedType
+import kotlin.reflect.jvm.jvmErasure
+import kotlin.reflect.typeOf
 
 open class SimulatorORMapping<E : Engine<E>> : ORMapping<E, List<DataRow>> {
     override fun getTypeFor(type: KType): Type<E> {
@@ -22,10 +25,29 @@ open class SimulatorORMapping<E : Engine<E>> : ORMapping<E, List<DataRow>> {
             @Suppress("UNCHECKED_CAST")
             return queryResult as List<T>
         } else {
-            return queryResult.map { row ->
-                val constructor = type.primaryConstructor ?: throw IllegalStateException("No primary constructor for ${type.simpleName}.")
-                constructor.callBy(constructor.parameters.associateWith { param -> row[param.name!!]})
-            }
+            return queryResult.map { row -> deserializeValue(row, type) as T }
         }
     }
+
+    private fun <T : Any> deserializeValue(value: Any?, type: KClass<T>): T? =
+        when {
+            value == null || type.isInstance(value) && type.starProjectedType != typeOf<Array<*>>() -> {
+                @Suppress("UNCHECKED_CAST")
+                value as? T
+            }
+            value is Array<*> && type.starProjectedType == typeOf<Array<*>>() -> {
+                val array = java.lang.reflect.Array.newInstance(type.java.componentType, value.size)
+                value.forEachIndexed { index, element ->
+                    @Suppress("UNCHECKED_CAST")
+                    (array as Array<Any?>)[index] = deserializeValue(element, type.java.componentType.kotlin)
+                }
+                @Suppress("UNCHECKED_CAST")
+                array as T
+            }
+            value is DataRow && type.isData -> {
+                val constructor = type.primaryConstructor ?: throw IllegalStateException("No primary constructor for ${type.simpleName}.")
+                constructor.callBy(constructor.parameters.associateWith { param -> deserializeValue(value[param.name!!], param.type.jvmErasure) })
+            }
+            else -> throw IllegalStateException("Cannot deserialize $value to $type.")
+        }
 }
