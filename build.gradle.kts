5f7674353dd30c958c779d1611d01131fedcf50a
Frieler, Christopher <christopher@frieler.rocks>
2025-09-22 21:11:11 +0200

    Implement sonar analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ allprojects {
     }
 }
 
+plugins {
+    alias(libs.plugins.sonarqube)
+}
+
 tasks.register("versionFile") {
     group = "build"
     description = "Writes the current package version to 'build/version.txt'."
@@ -20,3 +24,34 @@ tasks.register("versionFile") {
         }
     }
 }
+
+sonar {
+    properties {
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "kraftsql")
+        property("sonar.projectKey", "kraftsql_kraftsql-core")
+        property("sonar.projectName", "KraftSQL Core")
+        property("sonar.gradle.scanAll", "True")
+        if (System.getenv("GITHUB_ACTIONS") == "true") {
+            when (val githubEvent = System.getenv("GITHUB_EVENT_NAME")) {
+                "push" -> {
+                    property("sonar.branch.name", System.getenv("GITHUB_REF_NAME"))
+                }
+                "pull_request" -> {
+                    property("sonar.pullrequest.key", System.getenv("GITHUB_REF_NAME").removeSuffix("/merge"))
+                    property("sonar.scm.revision", System.getenv("GITHUB_PR_HEAD_SHA"))
+                }
+                else -> logger.warn("unknown GITHUB_EVENT_NAME '$githubEvent'")
+            }
+        }
+    }
+}
+project(":examples") {
+    sonar.isSkipProject = true
+}
+
+tasks.register<Exec>("fetchDefaultBranch") {
+    group = "other"
+    description = "Fetches the default branch of the current repository."
+    commandLine("git fetch --no-tags origin +refs/heads/${System.getenv("GITHUB_REPO_DEFAULT_BRANCH_NAME")}:refs/remotes/origin/${System.getenv("GITHUB_REPO_DEFAULT_BRANCH_NAME")}".split(" "))
+}.also { tasks.sonar.get().dependsOn(it) } // to enable comparison of the current branch with the default branch
