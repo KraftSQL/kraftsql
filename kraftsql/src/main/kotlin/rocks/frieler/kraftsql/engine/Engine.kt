f1f154bcde06ba45a51ea612e072c85ed8a572ec
Frieler, Christopher <christopher@frieler.rocks>
2025-10-04 16:21:13 +0200

    Add kdoc for Engine

--- kraftsql/src/main/kotlin/rocks/frieler/kraftsql/engine/Engine.kt
+++ kraftsql/src/main/kotlin/rocks/frieler/kraftsql/engine/Engine.kt
@@ -1,3 +1,11 @@
 package rocks.frieler.kraftsql.engine
 
+/**
+ * **Kraft**SQL's abstraction of an SQL engine.
+ *
+ * The interface itself is empty. Subtypes are used as type parameters to mark SQL stuff that runs on the same [Engine]
+ * and is thereby combinable in a statement.
+ *
+ * @param <E> compatible [Engine]s, usually the [Engine] itself
+ */
 interface Engine<E : Engine<E>>
