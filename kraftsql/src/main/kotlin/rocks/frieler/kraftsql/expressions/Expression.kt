c2edd07add5582ffeeb9a29f06acc1519dd4dd3f
Frieler, Christopher <christopher@frieler.rocks>
2025-10-08 21:21:36 +0200

    Write Kdoc for Expression

--- kraftsql/src/main/kotlin/rocks/frieler/kraftsql/expressions/Expression.kt
+++ kraftsql/src/main/kotlin/rocks/frieler/kraftsql/expressions/Expression.kt
@@ -2,9 +2,25 @@ package rocks.frieler.kraftsql.expressions
 
 import rocks.frieler.kraftsql.engine.Engine
 
+/**
+ * An SQL expression.
+ *
+ * @param <E> the [Engine] that implements and executes this [Expression]
+ * @param <T> the Kotlin type of the [Expression]'s value
+ */
 interface Expression<E: Engine<E>, T> {
+    /**
+     * Generates the SQL code for this [Expression].
+     *
+     * @return the SQL code for this [Expression]
+     */
     fun sql(): String
 
+    /**
+     * Returns the name of the resulting column, when this [Expression] is `SELECT`ed without an alias.
+     *
+     * @return the default column name  when `SELECT`ing this [Expression] without an alias
+     */
     fun defaultColumnName(): String
 
     override fun equals(other: Any?) : Boolean
