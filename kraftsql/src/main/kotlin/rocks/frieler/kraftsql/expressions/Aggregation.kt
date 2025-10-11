c71ce41bff7c9d8e8af8eef8ee4990432c7430a1
Frieler, Christopher <christopher@frieler.rocks>
2025-10-11 11:00:43 +0200

    Write Kdoc for Aggregation

--- kraftsql/src/main/kotlin/rocks/frieler/kraftsql/expressions/Aggregation.kt
+++ kraftsql/src/main/kotlin/rocks/frieler/kraftsql/expressions/Aggregation.kt
@@ -2,4 +2,10 @@ package rocks.frieler.kraftsql.expressions
 
 import rocks.frieler.kraftsql.engine.Engine
 
+/**
+ * An [Expression] that aggregates that calculates an aggregate value over multiple rows.
+ *
+ * @param <E> the [Engine] that implements and executes this [Aggregation]
+ * @param <T> the Kotlin type of the [Aggregation]'s result value
+ */
 interface Aggregation<E: Engine<E>, T> : Expression<E, T>
