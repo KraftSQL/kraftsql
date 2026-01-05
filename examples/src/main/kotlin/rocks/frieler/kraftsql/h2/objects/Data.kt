c991fef663c2e28f8bd40e50aef35ab577a5751e
Frieler, Christopher <christopher@frieler.rocks>
2026-01-05 10:24:42 +0100

    fixup! Introduce typealias for h2 Data

--- examples/src/main/kotlin/rocks/frieler/kraftsql/h2/objects/Data.kt
+++ examples/src/main/kotlin/rocks/frieler/kraftsql/h2/objects/Data.kt
@@ -1,10 +1,9 @@
 package rocks.frieler.kraftsql.h2.objects
 
 import rocks.frieler.kraftsql.h2.engine.H2Engine
-import rocks.frieler.kraftsql.objects.Data
 import rocks.frieler.kraftsql.objects.collect
 
-typealias Data<T> = Data<H2Engine, T>
+typealias Data<T> = rocks.frieler.kraftsql.objects.Data<H2Engine, T>
 
-inline fun <reified T : Any> rocks.frieler.kraftsql.h2.objects.Data<T>.collect() =
+inline fun <reified T : Any> Data<T>.collect() =
     collect(H2Engine.DefaultConnection.get())
