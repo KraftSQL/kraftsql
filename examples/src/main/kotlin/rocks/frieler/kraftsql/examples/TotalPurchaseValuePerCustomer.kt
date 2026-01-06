387f834783ab24fa19c9f3e70c8145276bc3e256
Frieler, Christopher <christopher@frieler.rocks>
2026-01-06 16:24:53 +0100

    [WIP] Add COUNT to example (where it counts NULLs that shouldn't count)

--- examples/src/main/kotlin/rocks/frieler/kraftsql/examples/TotalPurchaseValuePerCustomer.kt
+++ examples/src/main/kotlin/rocks/frieler/kraftsql/examples/TotalPurchaseValuePerCustomer.kt
@@ -8,9 +8,11 @@ import rocks.frieler.kraftsql.examples.data.purchases
 import rocks.frieler.kraftsql.examples.data.withSampleData
 import rocks.frieler.kraftsql.expressions.`=`
 import rocks.frieler.kraftsql.expressions.Coalesce
+import rocks.frieler.kraftsql.expressions.Count
 import rocks.frieler.kraftsql.expressions.Sum
 import rocks.frieler.kraftsql.h2.dsl.Select
 import rocks.frieler.kraftsql.h2.dsl.`as`
+import rocks.frieler.kraftsql.h2.engine.H2Engine
 import rocks.frieler.kraftsql.h2.expressions.Constant
 import rocks.frieler.kraftsql.h2.objects.Data
 import rocks.frieler.kraftsql.h2.objects.collect
@@ -24,7 +26,7 @@ fun main() {
     }
 }
 
-data class CustomerPurchaseValue(val customerId: Long, val totalAmount: BigDecimal)
+data class CustomerPurchaseValue(val customerId: Long, val purchases: Long, val totalAmount: BigDecimal)
 
 fun aggregatePurchaseValuePerCustomer(customers: Data<Customer>, purchases: Data<Purchase>) : Data<CustomerPurchaseValue> =
     Select {
@@ -32,6 +34,7 @@ fun aggregatePurchaseValuePerCustomer(customers: Data<Customer>, purchases: Data
         val p = leftJoin(purchases `as` "p") { this[Purchase::customerId] `=` c[Customer::id] }
         columns(
             c[Customer::id] `as` CustomerPurchaseValue::customerId,
+            Count<H2Engine>() `as` CustomerPurchaseValue::purchases,
             Coalesce(
                 Sum(p[Purchase::totalPrice]),
                 nonNullableExpression = Constant(BigDecimal.ZERO),
