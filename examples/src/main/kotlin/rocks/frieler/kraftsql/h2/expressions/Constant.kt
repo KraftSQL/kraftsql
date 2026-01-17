bb3bbfa5e49ab2933afec9d6ee86409ebec2e5ab
Frieler, Christopher <christopher@frieler.rocks>
2026-01-17 17:04:18 +0100

    Remove dead type parameter for Engine of h2 Constant

--- examples/src/main/kotlin/rocks/frieler/kraftsql/h2/expressions/Constant.kt
+++ examples/src/main/kotlin/rocks/frieler/kraftsql/h2/expressions/Constant.kt
@@ -1,10 +1,9 @@
 package rocks.frieler.kraftsql.h2.expressions
 
-import rocks.frieler.kraftsql.engine.Engine
 import rocks.frieler.kraftsql.h2.engine.H2Engine
 import rocks.frieler.kraftsql.h2.engine.Types
 
-open class Constant<E : Engine<E>, T>(
+open class Constant<T>(
     value: T,
 ) : rocks.frieler.kraftsql.expressions.Constant<H2Engine, T>(value) {
     override fun sql(): String {
