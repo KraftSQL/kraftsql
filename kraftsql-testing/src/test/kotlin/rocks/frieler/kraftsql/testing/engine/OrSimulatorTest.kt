354cc3351226efabb90b3d2721e582205bdd5b5f
Burgmaier, Aljoscha <aljoscha.burgmaier@dm.de>
2026-02-06 21:54:00 +0100

    implementation of the OrSimulator

---
+++ kraftsql-testing/src/test/kotlin/rocks/frieler/kraftsql/testing/engine/OrSimulatorTest.kt
@@ -0,0 +1,82 @@
+package rocks.frieler.kraftsql.testing.engine
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import rocks.frieler.kraftsql.expressions.Expression
+import rocks.frieler.kraftsql.expressions.Or
+import rocks.frieler.kraftsql.objects.DataRow
+
+class OrSimulatorTest {
+    private val subexpressionCallbacks = mock<ExpressionSimulator.SubexpressionCallbacks<DummyEngine>>()
+
+    @ParameterizedTest
+    @CsvSource(
+        "true, true, true",
+        "true, false, true",
+        "false, true, true",
+        "false, false, false",
+    )
+    fun `OrSimulator can simulate OR-operator to combine two expressions`(
+        left: Boolean,
+        right: Boolean,
+        expectedResult: Boolean
+    ) {
+        val row = mock<DataRow>()
+        val leftHandSide =
+            mock<Expression<DummyEngine, Boolean>>().also { whenever(subexpressionCallbacks.simulateExpression(it)).thenReturn { _ -> left } }
+        val rightHandSide =
+            mock<Expression<DummyEngine, Boolean>>().also { whenever(subexpressionCallbacks.simulateExpression(it)).thenReturn { _ -> right } }
+
+        val simulation = context(subexpressionCallbacks) {
+            OrSimulator<DummyEngine>().simulateExpression(Or(leftHandSide, rightHandSide))
+        }
+        val result = simulation(row)
+
+        result shouldBe expectedResult
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "true, NULL",
+            "NULL, true",
+        ], nullValues = ["NULL"]
+    )
+    fun `OrSimulator returns true when one side is NULL`(left: Boolean?, right: Boolean?) {
+        val row = mock<DataRow>()
+        val leftHandSide =
+            mock<Expression<DummyEngine, Boolean?>>().also { whenever(subexpressionCallbacks.simulateExpression(it)).thenReturn { _ -> left } }
+        val rightHandSide =
+            mock<Expression<DummyEngine, Boolean?>>().also { whenever(subexpressionCallbacks.simulateExpression(it)).thenReturn { _ -> right } }
+
+        val simulation = context(subexpressionCallbacks) {
+            OrSimulator<DummyEngine>().simulateExpression(Or(leftHandSide, rightHandSide))
+        }
+        val result = simulation(row)
+
+        result shouldBe true
+    }
+
+    @Test
+    fun `OrSimulator can simulate Or wrapping two aggregations`() {
+        val groupExpressions = emptyList<Expression<DummyEngine, *>>()
+        val row = mock<DataRow>()
+        val leftHandSide = mock<Expression<DummyEngine, Boolean>>().also {
+            context(groupExpressions) { whenever(subexpressionCallbacks.simulateAggregation(it)) }.thenReturn { _ -> true }
+        }
+        val rightHandSide = mock<Expression<DummyEngine, Boolean>>().also {
+            context(groupExpressions) { whenever(subexpressionCallbacks.simulateAggregation(it)) }.thenReturn { _ -> true }
+        }
+
+        val simulation = context(groupExpressions, subexpressionCallbacks) {
+            OrSimulator<DummyEngine>().simulateAggregation(Or(leftHandSide, rightHandSide))
+        }
+        val result = simulation(listOf(row))
+
+        result shouldBe true
+    }
+}
