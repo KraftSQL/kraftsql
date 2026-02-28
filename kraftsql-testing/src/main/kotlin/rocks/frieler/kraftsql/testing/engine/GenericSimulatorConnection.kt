dcb5bdcabaffd930b714395936f9f03530833b34
Frieler, Christopher <christopher@frieler.rocks>
2026-02-28 20:29:26 +0100

    [WIP] Implement less-or-equal comparison

--- kraftsql-testing/src/main/kotlin/rocks/frieler/kraftsql/testing/engine/GenericSimulatorConnection.kt
+++ kraftsql-testing/src/main/kotlin/rocks/frieler/kraftsql/testing/engine/GenericSimulatorConnection.kt
@@ -326,6 +326,7 @@ open class GenericSimulatorConnection<E : Engine<E>>(
         registerExpressionSimulator(CastSimulator<E, Any?>())
         registerExpressionSimulator(IsNotNullSimulator())
         registerExpressionSimulator(EqualsSimulator())
+        registerExpressionSimulator(LessOrEqualSimulator())
         registerExpressionSimulator(AndSimulator())
         registerExpressionSimulator(CoalesceSimulator<E, Any?>())
         registerExpressionSimulator(ArraySimulator<E, Any>())
