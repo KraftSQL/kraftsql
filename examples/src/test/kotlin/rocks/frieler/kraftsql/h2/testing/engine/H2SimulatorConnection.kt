293eeefbd4e444e122453f660ec3b9a5f3dee9d7
Frieler, Christopher <christopher@frieler.rocks>
2026-02-28 20:40:10 +0100

    Implement h2's SYSTEM_RANGE
    This will be used for a workaround to unnest array columns.

--- examples/src/test/kotlin/rocks/frieler/kraftsql/h2/testing/engine/H2SimulatorConnection.kt
+++ examples/src/test/kotlin/rocks/frieler/kraftsql/h2/testing/engine/H2SimulatorConnection.kt
@@ -2,11 +2,21 @@ package rocks.frieler.kraftsql.h2.testing.engine
 
 import rocks.frieler.kraftsql.h2.engine.H2Engine
 import rocks.frieler.kraftsql.h2.expressions.ArrayConcatenation
+import rocks.frieler.kraftsql.h2.expressions.SystemRange
+import rocks.frieler.kraftsql.h2.objects.Data
+import rocks.frieler.kraftsql.objects.DataRow
 import rocks.frieler.kraftsql.testing.engine.ArrayConcatenationSimulator
 import rocks.frieler.kraftsql.testing.engine.GenericSimulatorConnection
 import kotlin.reflect.KClass
 
 class H2SimulatorConnection : GenericSimulatorConnection<H2Engine>(orm = H2SimulatorORMapping) {
+
+    override fun fetchData(data: Data<*>, correlatedData: DataRow?) =
+        when (data) {
+            is SystemRange -> (simulateExpression(data.from)(DataRow())..simulateExpression(data.to)(DataRow())).map { DataRow("X" to it) }
+            else -> super.fetchData(data, correlatedData)
+        }
+
     init {
         unregisterExpressionSimulator(rocks.frieler.kraftsql.expressions.Constant::class)
         registerExpressionSimulator(ConstantSimulator<Any?>())
