692da699b6e90bba0a94800f6b8fb22cc44d2551
Frieler, Christopher <christopher@frieler.rocks>
2026-04-03 22:10:48 +0200

    Migrate new MIN tests to mockk
    ...to gain more experience with it. Hence, no full migration yet.

--- kraftsql-testing/src/test/kotlin/rocks/frieler/kraftsql/testing/simulator/expressions/MinSimulatorTest.kt
+++ kraftsql-testing/src/test/kotlin/rocks/frieler/kraftsql/testing/simulator/expressions/MinSimulatorTest.kt
@@ -3,8 +3,8 @@ package rocks.frieler.kraftsql.testing.simulator.expressions
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
+import io.mockk.mockk
+import io.mockk.every
 import rocks.frieler.kraftsql.expressions.Expression
 import rocks.frieler.kraftsql.expressions.Min
 import rocks.frieler.kraftsql.objects.DataRow
@@ -12,23 +12,24 @@ import rocks.frieler.kraftsql.testing.simulator.engine.DummyEngine
 import java.sql.SQLException
 
 class MinSimulatorTest {
-    private val subexpressionCallbacks = mock<ExpressionSimulator.SubexpressionCallbacks<DummyEngine>>()
+    private val subexpressionCallbacks = mockk<ExpressionSimulator.SubexpressionCallbacks<DummyEngine>>()
 
     private val minSimulator = MinSimulator<DummyEngine, Comparable<Comparable<*>>>()
 
     @Test
     fun `MinSimulator rejects simulation as Expression`() {
         shouldThrow<SQLException> {
-            context(subexpressionCallbacks) { minSimulator.simulateExpression(Min(mock())) }
+            context(subexpressionCallbacks) { minSimulator.simulateExpression(Min(mockk())) }
         }
     }
 
     @Test
     fun `MinSimulator can simulate Min aggregation`() {
-        val expression = mock<Expression<DummyEngine, Int>>()
-        val row1 = mock<DataRow>()
-        val row2 = mock<DataRow>()
-        whenever(subexpressionCallbacks.simulateExpression(expression)).thenReturn { row -> if (row == row1) 1 else if (row == row2) 2 else error("unexpected row") }
+        val expression = mockk<Expression<DummyEngine, Int>>()
+        val expressionSimulation = mockk<Function1<DataRow, Int>>()
+        every { subexpressionCallbacks.simulateExpression(expression) } returns expressionSimulation
+        val row1 = mockk<DataRow>().also { every { expressionSimulation.invoke(it) } returns 1 }
+        val row2 = mockk<DataRow>().also { every { expressionSimulation.invoke(it) } returns 2 }
         @Suppress("UNCHECKED_CAST") val min = Min(expression) as Min<DummyEngine, Comparable<Comparable<*>>>
 
         val simulation = context(subexpressionCallbacks, emptyList<Expression<DummyEngine, *>>()) {
@@ -41,10 +42,11 @@ class MinSimulatorTest {
 
     @Test
     fun `MinSimulator ignores NULL values`() {
-        val expression = mock<Expression<DummyEngine, Int?>>()
-        val rowWithValue = mock<DataRow>()
-        val rowWithNull = mock<DataRow>()
-        whenever(subexpressionCallbacks.simulateExpression(expression)).thenReturn { row -> if (row == rowWithValue) 1 else null }
+        val expression = mockk<Expression<DummyEngine, Int?>>()
+        val expressionSimulation = mockk<Function1<DataRow, Int?>>()
+        every { subexpressionCallbacks.simulateExpression(expression) } returns expressionSimulation
+        val rowWithValue = mockk<DataRow>().also { every { expressionSimulation.invoke(it) } returns 1 }
+        val rowWithNull = mockk<DataRow>().also { every { expressionSimulation.invoke(it) } returns null }
         @Suppress("UNCHECKED_CAST") val min = Min(expression) as Min<DummyEngine, Comparable<Comparable<*>>>
 
         val simulation = context(subexpressionCallbacks, emptyList<Expression<DummyEngine, *>>()) {
@@ -57,10 +59,11 @@ class MinSimulatorTest {
 
     @Test
     fun `MinSimulator returns NULL as minimum of only NULL values`() {
-        val expression = mock<Expression<DummyEngine, Int?>>()
-        whenever(subexpressionCallbacks.simulateExpression(expression)).thenReturn { _ -> null }
-        val row1 = mock<DataRow>()
-        val row2 = mock<DataRow>()
+        val expression = mockk<Expression<DummyEngine, Int?>>()
+        val expressionSimulation = mockk<Function1<DataRow, Int?>>()
+        every { subexpressionCallbacks.simulateExpression(expression) } returns expressionSimulation
+        val row1 = mockk<DataRow>().also { every { expressionSimulation.invoke(it) } returns null }
+        val row2 = mockk<DataRow>().also { every { expressionSimulation.invoke(it) } returns null }
         @Suppress("UNCHECKED_CAST") val min = Min(expression) as Min<DummyEngine, Comparable<Comparable<*>>>
 
         val simulation = context(subexpressionCallbacks, emptyList<Expression<DummyEngine, *>>()) {
@@ -73,8 +76,9 @@ class MinSimulatorTest {
 
     @Test
     fun `MinSimulator returns NULL as minimum of empty data`() {
-        val expression = mock<Expression<DummyEngine, Int?>>()
-        whenever(subexpressionCallbacks.simulateExpression(expression)).thenReturn { _ -> null }
+        val expression = mockk<Expression<DummyEngine, Int?>>()
+        val expressionSimulation = mockk<Function1<DataRow, Int?>>()
+        every { subexpressionCallbacks.simulateExpression(expression) } returns expressionSimulation
         @Suppress("UNCHECKED_CAST") val min = Min(expression) as Min<DummyEngine, Comparable<Comparable<*>>>
 
         val simulation = context(subexpressionCallbacks, emptyList<Expression<DummyEngine, *>>()) {
--- kraftsql/src/test/kotlin/rocks/frieler/kraftsql/expressions/MinTest.kt
+++ kraftsql/src/test/kotlin/rocks/frieler/kraftsql/expressions/MinTest.kt
@@ -3,16 +3,16 @@ package rocks.frieler.kraftsql.expressions
 import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.equals.shouldNotBeEqual
 import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
 import rocks.frieler.kraftsql.engine.TestableDummyEngine
 
 class MinTest {
     @Test
     fun `SQL calls MIN with expression`() {
-        val expression = mock<Expression<TestableDummyEngine, Int?>> {
-            whenever(it.sql()).thenReturn("x")
+        val expression = mockk<Expression<TestableDummyEngine, Int?>> {
+            every { sql() } returns "x"
         }
 
         val sql = Min(expression).sql()
@@ -22,8 +22,8 @@ class MinTest {
 
     @Test
     fun `default column name renders MIN() with argument's default column name`() {
-        val expression = mock<Expression<TestableDummyEngine, Int?>> {
-            whenever(it.defaultColumnName()).thenReturn("x")
+        val expression = mockk<Expression<TestableDummyEngine, Int?>> {
+            every { defaultColumnName() } returns "x"
         }
 
         val sql = Min(expression).defaultColumnName()
@@ -33,27 +33,27 @@ class MinTest {
 
     @Test
     fun `Min with equal argument is equal`() {
-        val expression = mock<Expression<TestableDummyEngine, Int?>>()
+        val expression = mockk<Expression<TestableDummyEngine, Int?>>()
 
         Min(expression) shouldBeEqual Min(expression)
     }
 
     @Test
     fun `Min with different argument is not equal`() {
-        val expression1 = mock<Expression<TestableDummyEngine, Int?>>()
-        val expression2 = mock<Expression<TestableDummyEngine, Int?>>()
+        val expression1 = mockk<Expression<TestableDummyEngine, Int?>>()
+        val expression2 = mockk<Expression<TestableDummyEngine, Int?>>()
 
         Min(expression1) shouldNotBeEqual Min(expression2)
     }
 
     @Test
     fun `Min and something else are not equal`() {
-        Min<TestableDummyEngine, Int>(mock()) shouldNotBeEqual Any()
+        Min<TestableDummyEngine, Int>(mockk()) shouldNotBeEqual Any()
     }
 
     @Test
     fun `Min with equal arguments have same hash code`() {
-        val expression = mock<Expression<TestableDummyEngine, Int?>>()
+        val expression = mockk<Expression<TestableDummyEngine, Int?>>()
 
         Min(expression).hashCode() shouldBeEqual Min(expression).hashCode()
     }
